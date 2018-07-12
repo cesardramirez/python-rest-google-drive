e4250ce4167bce7a987f46be1298d4c41f86d54e
cesardramirez <cesardavid89@gmail.com>
2018-07-12 16:58:25 -0500

    feat(insertFileFolder-searchFile): Subir un archivo a una carpeta específica y buscar un archivo según su nombre.

--- operations.py
+++ operations.py
@@ -25,18 +25,18 @@ class Operations:
             for item in items:
                 print('{0} ({1})'.format(item['name'], item['id']))
 
-    def upload_file(self, filename, filepath, mimetype):
+    def upload_file(self, filename, file_path, mime_type):
         drive_service = Auth().get_service()
 
         file_metadata = {'name': filename}
-        media = MediaFileUpload(filepath, mimetype=mimetype)
+        media = MediaFileUpload(file_path, mimetype=mime_type)
         file = drive_service.files().create(body=file_metadata,
                                                            media_body=media,
                                                            fields='id').execute()
         print('Archivo cargado! ')
         print('ID Archivo: %s' % file.get('id'))
 
-    def download_file(self, file_id, filepath):
+    def download_file(self, file_id, file_path):
         drive_service = Auth().get_service()
 
         request = drive_service.files().get_media(fileId=file_id)
@@ -46,21 +46,52 @@ class Operations:
         while done is False:
             status, done = downloader.next_chunk()
             print("Download %d%%." % int(status.progress() * 100))
-        with io.open(filepath, 'wb') as f:
+        with io.open(file_path, 'wb') as f:
             fh.seek(0)
             f.write(fh.read())
             print('Archivo %s descargado!' % file_id)
 
-    def create_folder(self, foldername):
+    def create_folder(self, folder_name):
         drive_service = Auth().get_service()
 
         file_metadata = {
-            'name': foldername,
+            'name': folder_name,
             'mimeType': 'application/vnd.google-apps.folder'
         }
         file = drive_service.files().create(body=file_metadata,
                                             fields='id').execute()
         print('Carpeta Creada!')
         print('ID Carpeta: %s' % file.get('id'))
+
+    def insert_file_to_folder(self, folder_id, file_name, file_path, mime_type):
+        drive_service = Auth().get_service()
+
+        file_metadata = {
+            'name': file_name,
+            'parents': [folder_id]
+        }
+        media = MediaFileUpload(file_path,
+                                mimetype=mime_type,
+                                resumable=True)
+        file = drive_service.files().create(body=file_metadata,
+                                            media_body=media,
+                                            fields='id').execute()
+        print('ID Archivo %s colocado en ID Folder %s' % (file.get('id'), folder_id))
+
+    def search_file(self, query):
+        drive_service = Auth().get_service()
+
+        page_token = None
+        while True:
+            response = drive_service.files().list(q=query,
+                                                  spaces='drive',
+                                                  fields='nextPageToken, files(id, name)',
+                                                  pageToken=page_token).execute()
+            for file in response.get('files', []):
+                # Process change
+                print('Archivo encontrado: %s (%s)' % (file.get('name'), file.get('id')))
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
 
 
